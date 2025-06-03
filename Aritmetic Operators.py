03c147584892ed3e8dc2935010c9b33d4be5238a
Shashank876747 <shashank.dhak27@gmail.com>
2025-06-03 14:10:32 -0400

    Initial code

---
+++ Aritmetic Operators.py
@@ -0,0 +1,9 @@
+# Aritmetic Operators
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a + b)      # Sum
+    print(a - b)      # Difference
+    print(a * b)      # Product
